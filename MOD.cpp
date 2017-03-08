0a570bb9aa2af08e5fd1ef8213f3fe1e5ce88251
Durgesh Soni <durgesh9491@gmail.com>
2017-03-09 00:58:29 +0530

    UPD- HE

---
+++ MOD.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LL    long long
+
+class MOD{
+	private: 
+		LL mod;
+		bool isPrime;
+		const static LL INF = 2e18;
+		
+	public:
+		MOD(){
+			mod = 1e9 + 7;
+			isPrime = true;
+		}
+		MOD(LL &ref, LL &refPrime){
+			mod = ref;
+			isPrime = refPrime;
+		}
+		LL add(const LL &a, const LL &b){
+			LL x = a + b;
+			return (x >= mod) ? (x-mod) : x;
+		}
+		LL sub(const LL &a, const LL &b){
+			int x = a - b;
+			return (x < 0) ? (x  + mod) : x;
+		}
+		LL mul(const LL &a, const  LL &b){
+			LL x = (a * b);
+			return (x >= mod) ? (x % mod) : x;
+		}
+		
+		LL modexp(LL a,LL b){
+			LL x = 1; a = add(a, 0);
+			while(b > 0){
+				if(b & 1)
+					x = mul(x, a);
+				a = mul(a, a);
+			    b >>= 1;
+			}
+			return x;
+	    }
+	    
+		LL mulmod(LL a,LL b){
+			LL x = 0; a = add(a, 0); 
+			while(b > 0){
+				if(b&1)
+					x = add(x, a);
+				a = add(a, a);
+				b >>= 1;
+			}
+			return x;
+		}
+		
+		LL div(LL &a, LL &b){
+			return (b < INF/mod) ? ((a % mul(b, mod)) / b) : mul(a, modexp(b, mod - 2));
+		}
+};
+
+
+
+
+int main(){
+	return 0;
+}
